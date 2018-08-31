1ba1e0c4a2dc5930e6f30de2b8d04599468a4075
İsmail <ismyilmaz@gmail.com>
2018-08-31 23:14:38 +0300

    Project Added

---
+++ SehirRehberi.API/Data/AppRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SehirRehberi.API.Models;
+
+namespace SehirRehberi.API.Data
+{
+    public class AppRepository : IAppRepository
+    {
+        private readonly DataContext _context;
+
+        public AppRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+        public bool SaveAll()
+        {
+            return _context.SaveChanges() > 0;
+        }
+
+        public List<City> GetCities()
+        {
+            return _context.Cities.Include(c => c.Photos).ToList();
+
+        }
+
+        public List<Photo> GetPhotosByCity(int cityId)
+        {
+            return _context.Photos.Where(p => p.CityId == cityId).ToList();
+        }
+
+        public City GetCityById(int cityId)
+        {
+            return _context.Cities.Include(c => c.Photos).FirstOrDefault(c => c.Id == cityId);
+        }
+
+        public Photo GetPhoto(int id)
+        {
+            return _context.Photos.FirstOrDefault(p => p.Id == id);
+        }
+
+    }
+}
---
+++ SehirRehberi.API/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using SehirRehberi.API.Data;
+
+namespace SehirRehberi.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var key = Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value);
+            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddAutoMapper();
+            //AddJsonOptions Reference Looping Hatası için eklendi.
+            services.AddMvc().AddJsonOptions(opt =>
+                {
+                    opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                });
+
+            //services.AddCors();
+            services.AddScoped<IAppRepository, AppRepository>();
+            // JWT konfigürasyonu
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
